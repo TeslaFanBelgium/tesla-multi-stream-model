99877cae43d307d71a6ce8becd4f82fc261bee92
TeslaFanBelgium <kurt.missotten@gmail.com>
2026-02-26 19:45:15 +0100

    Update cache name to tesla-model-v7

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'tesla-model-v6';
+const CACHE_NAME = 'tesla-model-v7';
 const urlsToCache = [
-  '/index.html',
-  '/manifest.json',
-  '/sw.js',
+  'index.html',
+  'manifest.json',
+  'sw.js',
   'https://cdn.jsdelivr.net/npm/chart.js@4.4.4/dist/chart.umd.min.js'
 ];
 
