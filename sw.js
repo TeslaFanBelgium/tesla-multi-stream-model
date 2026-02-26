3b1aac2271bcc4c8c5ea17d4298e71d1952e750a
TeslaFanBelgium <kurt.missotten@gmail.com>
2026-02-26 19:18:11 +0100

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'tesla-model-v6';
+const urlsToCache = [
+  '/index.html',
+  '/manifest.json',
+  '/sw.js',
+  'https://cdn.jsdelivr.net/npm/chart.js@4.4.4/dist/chart.umd.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
